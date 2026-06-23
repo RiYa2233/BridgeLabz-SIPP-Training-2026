b79c5b5c62afc7f4eb0467f165b9409d4e8ebecc
RiYa2233 <kakolisengupta595@gmail.com>
2026-06-23 13:28:04 +0530

    riya

--- OOPS/oops/oops_fundamental_programming/BankAccount.java
+++ OOPS/oops/oops_fundamental_programming/BankAccount.java
@@ -1,4 +1,4 @@
-package OOPS.oops.oops_fundamental_programming;
+package oops.oops_fundamental_programming;
 
 public class BankAccount {
 
@@ -8,7 +8,7 @@ public class BankAccount {
 
     static int totalAccounts = 0;
 
-    BankAccount(int accountNumber, String holder, double balance) {
+    public BankAccount(int accountNumber, String holder, double balance) {
         this.accountNumber = accountNumber;
         this.holder = holder;
         this.balance = balance;
@@ -40,21 +40,18 @@ public class BankAccount {
         BankAccount a2 = new BankAccount(102, "Aman", 15000);
         BankAccount a3 = new BankAccount(103, "Rohit", 20000);
 
-        // 5 transactions for Account 1
         a1.deposit(1000);
         a1.withdraw(500);
         a1.deposit(2000);
         a1.withdraw(1000);
         a1.deposit(500);
 
-        // 5 transactions for Account 2
         a2.deposit(1500);
         a2.withdraw(1000);
         a2.deposit(500);
         a2.withdraw(2000);
         a2.deposit(1000);
 
-        // 5 transactions for Account 3
         a3.deposit(3000);
         a3.withdraw(1000);
         a3.deposit(2000);
--- OOPS/oops/oops_fundamental_programming/ChargingStation.java
+++ OOPS/oops/oops_fundamental_programming/ChargingStation.java
@@ -2,27 +2,22 @@ package OOPS.oops.oops_fundamental_programming;
 
 public class ChargingStation {
 
-    // Static Variables
     static int totalStations = 0;
     static double electricityRate = 8.0;
 
-    // Instance Variables
     int stationId;
     double unitsConsumed;
 
-    // Constructor
-    ChargingStation(int stationId, double unitsConsumed) {
+    public ChargingStation(int stationId, double unitsConsumed) {
         this.stationId = stationId;
         this.unitsConsumed = unitsConsumed;
         totalStations++;
     }
 
-    // Calculate Bill
     double calculateBill() {
         return unitsConsumed * electricityRate;
     }
 
-    // Display Details
     void displayStationDetails() {
         System.out.println("Station ID: " + stationId);
         System.out.println("Units Consumed: " + unitsConsumed);
@@ -38,7 +33,6 @@ public class ChargingStation {
         ChargingStation s4 = new ChargingStation(104, 180);
         ChargingStation s5 = new ChargingStation(105, 200);
 
-        // Change electricity rate for all stations
         electricityRate = 10.0;
 
         s1.displayStationDetails();
